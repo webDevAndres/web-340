f366dbb3723f81b9dc1d6002407270e384b82924
Andres Macias <macias.andres02@gmail.com>
2022-06-12 22:23:08 -0500

    main js file

---
+++ week-3/sports-app/index.js
@@ -0,0 +1,28 @@
+const teamManager = require("./team-manager");
+
+function displayTeams() {
+    let arrayOfTeams = teamManager.findTeams();
+    let teams = "-- DISPLAYING TEAMS --";
+   arrayOfTeams.forEach(team => {
+        teams += "\n";
+        teams += "Name: " + team.name + "\n";
+        teams += "Mascot: " + team.mascot + "\n";
+        teams += "Player Count: " + team.playerCount;
+        teams += "\n"
+    });
+
+    return teams;
+}
+
+function displayChampionshipGame() {
+    // let randomTeam = Math.floor(Math.random() * 5);
+    let teamOne = teamManager.findTeam("Penn State");
+    let teamTwo = teamManager.findTeam("Iowa State");
+    let championshipGameString = "--CHAMPIONSHIP GAME --";
+    championshipGameString += "\n";
+    championshipGameString += teamManager.getGame(teamOne, teamTwo);
+    return championshipGameString;
+
+}
+
+console.log(displayTeams(), "\n", displayChampionshipGame());
