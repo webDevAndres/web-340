e62702895c26744c19e4a82752e16dbd5efa452d
Andres Macias <macias.andres02@gmail.com>
2022-06-12 22:23:20 -0500

    team class

---
+++ week-3/sports-app/team.js
@@ -0,0 +1,9 @@
+class Team {
+    constructor(name, mascot, playerCount) {
+        this.name = name;
+        this.mascot = mascot;
+        this.playerCount = playerCount;
+    }
+}
+
+module.exports = Team;
